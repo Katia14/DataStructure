c79d32f92669ad1e96208f0674b9a07be3431b6e
Katia14 <ekaterrina@gmail.com>
2021-03-03 20:18:17 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,64 +6,64 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> objectList = new ArrayList<>();
-        objectList.add("Computer");
-        objectList.add("Plate");
-        objectList.add("Chair");
-        objectList.add("Girl");
-        objectList.add("Boy");
-        objectList.add("Cat");
-        objectList.add("Dog");
-        objectList.add("Determination");
+        List<String> randomWords = new ArrayList<>();
+        randomWords.add("Computer");
+        randomWords.add("Plate");
+        randomWords.add("Chair");
+        randomWords.add("Girl");
+        randomWords.add("Boy");
+        randomWords.add("Cat");
+        randomWords.add("Dog");
+        randomWords.add("Determination");
 
         int startsWithC = 0;
         int endsWithE = 0;
         int containsE = 0;
         int length5 = 0;
 
-        for (String value : objectList) {
+        for (String value : randomWords) {
             if (value.startsWith("C")) {
                 startsWithC++;
             }
         }
         System.out.println("\nThere are " + startsWithC + " words starting with 'C' ");
 
-        for (String value : objectList) {
+        for (String value : randomWords) {
             if (value.endsWith("e")) {
                 endsWithE++;
             }
         }
         System.out.println("\nThere is " + endsWithE + " word ending with 'e' ");
 
-        for (String value : objectList) {
+        for (String value : randomWords) {
             if (value.contains("e")) {
                 containsE++;
             }
         }
         System.out.println("\nThere are " + containsE + " words contains 'e' ");
 
-        for (String value : objectList) {
+        for (String value : randomWords) {
             if (value.length() == 5) {
                 length5++;
             }
         }
         System.out.println("\nThere are " + length5 + " words with length '5' ");
 
-        for (String value : objectList) {
+        for (String value : randomWords) {
             if (value.contains("te")) ;
         }
         System.out.println("\nThere are words containing 'te' ");
 
         System.out.println("\nBefore Sorting:");
-        for (String counter : objectList) {
+        for (String counter : randomWords) {
             System.out.println(counter);
         }
 
         // Sort statement
-        Collections.sort(objectList);
+        Collections.sort(randomWords);
 
            System.out.println("\nAfter Sorting:");
-        for (String counter : objectList) {
+        for (String counter : randomWords) {
             System.out.println(counter);
         }
     }
